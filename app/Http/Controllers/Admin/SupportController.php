26edd2f2ca921eced2c985e6f4e03f2d8274df54
Antonio Gutierrez da Conceicao <antonio@phpmaster.com.br>
2023-10-18 09:56:50 +0000

    Diversos tipos de chamado a visao pelo SupportController

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin.supports.index');
+//        $supports = Support::all();
+//        $support = new Support();
+        $supports = $support->all();
+
+//        dd($supports);
+
+        return view('admin.supports.index', [
+            'supports' => $supports
+        ]);
     }
 }
